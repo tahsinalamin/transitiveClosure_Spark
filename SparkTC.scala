4f109c1519c087b918a37d8daffab2b908b6f091
Sikder Tahsin Al-Amin <sal-amin2@uh.edu>
2018-05-08 13:57:55 -0500

    Update SparkTC.scala

--- SparkTC.scala
+++ SparkTC.scala
@@ -1,5 +1,3 @@
-//Author: Sikder Tahsin Al-Amin
-
 import scala.util.Random                                      
 import scala.collection.mutable  
 import scala.io.StdIn.{readLine, readInt}                             
@@ -14,8 +12,11 @@ import scala.collection.mutable.ArrayBuffer;
                                                               
 import org.apache.log4j.Logger                                
 import org.apache.log4j.Level                                 
-                                                        
-object SparkTC {     
+/**                                                           
+ * Transitive closure on a graph.                             
+ */                                                           
+object SparkTC {                                                                                                                                                                                                      
+                                                            
   def main(args: Array[String]) {                             
     Logger.getLogger("org").setLevel(Level.OFF)               
     Logger.getLogger("akka").setLevel(Level.OFF)    
@@ -25,9 +26,9 @@ object SparkTC {
     val sparkConf = new SparkConf().setAppName("SparkTC")     
     val spark = new SparkContext(sparkConf) 
 
-    /////process the input file - first put the file in hadoop system
+    /////process the input file
     val fs = FileSystem.get(spark.hadoopConfiguration)
-    val stream = fs.open(new Path("/graph/minigraph.txt"))
+    val stream = fs.open(new Path("/graph/treecliquelinear995424.txt"))
     var line = stream.readLine()
     var data: mutable.Set[(Int, Int)] = mutable.Set.empty   
 
@@ -39,8 +40,15 @@ object SparkTC {
     stream.close() 
                                                               
     val t1 = System.nanoTime           ////starting time                       
-                                                          
+             
+    //var generateGraph = GraphLoader.edgeListFile(spark, "/graph/minigraph.txt",true).partitionBy(PartitionStrategy.RandomVertexCut)               
+    //val edges2=Set((3,0), (1,2), (4,1), (3,1), (0,2), (4,2), (2,0), (1,0), (2,1), (1,3))
     var tc = spark.parallelize(data.toSeq).cache() 
+
+    // Linear transitive closure: each round grows paths by one edge,
+    // by joining the graph's edges with the already-discovered paths.
+    // e.g. join the path (y, z) from the TC with the edge (x, y) from
+    // the graph to obtain the path (x, z).
 
     // Because join() joins on keys, the edges are stored in reversed order.
     val edges = tc.map(x => (x._2, x._1))
@@ -48,7 +56,7 @@ object SparkTC {
 
     // This join is iterated until a fixed point is reached.
     var tc_temp = tc
-    //do {
+    
     for( i <- 2 to depth)
     {
       // Perform the join, obtaining an RDD of (y, (z, x)) pairs,
@@ -56,27 +64,29 @@ object SparkTC {
       if(i==2)
       {
         //tc_temp=tc_temp.map(x => (x._2, x._1))
-        tc_temp = tc_temp.join(edges).map(x => (x._2._2, x._2._1)).distinct().cache()  //get table R2
+        tc_temp = tc_temp.join(edges).map(x => (x._2._2, x._2._1)).distinct().cache()
         //println("2 executed")
       }
       else if (i==3)
       {
         tc_temp=tc_temp.map(x => (x._2, x._1))
         tc_temp = tc_temp.filter(x=>x._1 != x._2) //removing the circles 
-        tc_temp = tc_temp.join(tc_copy).map(x => (x._2._2, x._2._1)).distinct().cache() ///get Table R3
+        tc_temp = tc_temp.join(tc_copy).map(x => (x._2._2, x._2._1)).distinct().cache() ///intermediate tables like r2,r3...
         //println("3 executed")
       }
       else 
       {
         tc_temp = tc_temp.filter(x=>x._1 != x._2) //removing the circles 
-        println("After eliminating circle, "+i+" edges = " + tc_temp.count())
-        tc_temp = tc_temp.join(tc_copy).map(x => (x._2._2, x._2._1)).distinct().cache() ///get tables like R4,R5...
+        //println("After eliminating circle, "+i+" edges = " + tc_temp.count())
+        tc_temp = tc_temp.join(tc_copy).map(x => (x._2._2, x._2._1)).distinct().cache() ///intermediate tables like r2,r3...
       }
 
-      println("Tc_temp "+i+" edges = " + tc_temp.count())
-      tc=tc.union(tc_temp) // R = R1 U R2 U...
-      println(tc.count())
-    
+      //println("Tc_temp "+i+" edges = " + tc_temp.count())
+      tc=tc.union(tc_temp) // R1 U R2 U...
+      //println(tc.collect())
+      //nextCount = tc.count()
+      //tc = tc.filter(x=>x._1 != x._2)
+      //println(tc.count())   
     }
     
     val duration = (System.nanoTime - t1) / 1e9d
@@ -88,4 +98,3 @@ object SparkTC {
     spark.stop()
   }
 }
-
